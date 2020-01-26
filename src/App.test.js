ba6863765e19cbfcff3135166ee9f84c3a1137f5
Mazen <Mazen@Mazen.local>
2020-01-26 04:19:31 +0200

    Fix testing lazy loaded components

--- src/App.test.js
+++ src/App.test.js
@@ -1,46 +1,33 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { MemoryRouter, Route } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header/Header';
-import Home from './views/Home/Home';
-import JobDetails from './views/JobDetails/JobDetails';
+import { createMemoryHistory } from 'history';
+import '@testing-library/jest-dom/extend-expect';
+import { render, wait, cleanup, waitForElement, getByTestId } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import NotFound from './views/NotFound/NotFound';
 
-configure({ adapter: new Adapter() });
+afterEach(cleanup);
 
-let pathMap = {};
+const renderWithRouter = (children, historyConf = {}) => {
+  const history = createMemoryHistory(historyConf)
+  return render(<Router history={history}>{children}</Router>)
+}
 
 /**
  * Routes test suite
  */
 describe('<App/>', ()=> {
-    beforeAll(() => {
-      const component = shallow(<App/>);
-      pathMap = component.find(Route).reduce((pathMap, route) => {
-          const routeProps = route.props();
-          pathMap[routeProps.path] = routeProps.component;
-          return pathMap;
-        }, {});
-    })
+  test('renders lazy Header component ', async ()=> {
+    const { getByTitle } = render(<App />);
 
-    it('should show Home view for /jobs route', () => {
-      expect(pathMap['/jobs']).toBe(Home);
-    })
-    it('should show JobDetails view for /jobs/:id route', () => {
-      expect(pathMap['/jobs/:id']).toBe(JobDetails);
-    })
-    it('should show NotFound for /404 route', () => {
-      expect(pathMap['/404']).toBe(NotFound);
-    })
+    const lazyElement = await waitForElement(()=> getByTitle(/HeyJobs/))
+    expect(lazyElement).toBeInTheDocument();
+  });
 
-    it('should show Header component appended in all routes', () => {
-      const wrapper = mount(
-          <MemoryRouter initialEntries={['/']}>
-              <App />
-          </MemoryRouter>
-      )
-      expect(wrapper.find(Header)).toHaveLength(1);
-  })
+  test('renders lazy Home component ', async ()=> {
+    const { getByText } = render(<App />);
+
+    const lazyElement = await waitForElement(()=> getByText(/Come join us at HeyJobs/))
+    expect(lazyElement).toBeInTheDocument();
+  });
 })
